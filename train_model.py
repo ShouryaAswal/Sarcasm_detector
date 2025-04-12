8b680f95628ad393f7a9722b23286803155c74a3
ShouryaAswal <146464091+ShouryaAswal@users.noreply.github.com>
2025-04-12 22:31:06 +0530

    fix model compatibility issue

--- train_model.py
+++ train_model.py
@@ -4,10 +4,7 @@ import os
 
 os.makedirs("app/model", exist_ok=True)
 
-
-pipe = pipeline("text-classification", model="mrm8488/t5-base-finetuned-sarcasm-twitter")
-
-
+pipe = pipeline("text-classification", model="AriEM/sarcasm-detector-roberta")
 joblib.dump(pipe, "app/model/sentiment_pipeline.pkl")
 
-print("Model saved to app/model/sentiment_pipeline.pkl")
+print("✅ Model saved to app/model/sentiment_pipeline.pkl")
