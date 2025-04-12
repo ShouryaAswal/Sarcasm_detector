b77cb6966af820a520921a087f65db309199568a
ShouryaAswal <146464091+ShouryaAswal@users.noreply.github.com>
2025-04-12 20:55:19 +0530

    improved predicted emotion in main

--- app/main.py
+++ app/main.py
@@ -1,22 +1,34 @@
-# app/main.py
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import logging
 
-# Set up logging
-logging.basicConfig(filename="predictions.log", level=logging.INFO)
-
 app = FastAPI()
-
-# Load model pipeline
 pipe = joblib.load("app/model/sentiment_pipeline.pkl")
+
+# Label mapping
+label_map = {
+    "LABEL_0": "Negative",
+    "LABEL_1": "Neutral",
+    "LABEL_2": "Positive"
+}
 
 class InputText(BaseModel):
     text: str
 
 @app.post("/predict")
 def predict(input: InputText):
-    result = pipe(input.text)
-    logging.info(f"Input: {input.text} | Prediction: {result}")
-    return {"input": input.text, "prediction": result}
+    raw_prediction = pipe(input.text)[0]
+    readable = {
+        "sentiment": label_map.get(raw_prediction["label"], "Unknown"),
+        "confidence": round(raw_prediction["score"], 3)
+    }
+
+    # Log prediction
+    logging.basicConfig(filename="predictions.log", level=logging.INFO)
+    logging.info(f"Input: {input.text} | Prediction: {readable}")
+
+    return {
+        "input": input.text,
+        "prediction": readable
+    }
